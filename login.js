382383cc5539c1ee5a217f0e3404933bc9448d49
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-03-08 22:43:47 +0600

    JavaScript file created for login page and setup for login to go homepage.

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    if (username == "admin" && password == "admin123") {
+        alert('Login SuccessFull')
+        window.location.replace("/homepage.html")
+    } else {
+        alert('Wrong Password')
+    }
+})
